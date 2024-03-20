10794aba0a57dddfdf8a4c768063da753e0bd684
ikad <tokunaga.c.daigo@forgxisto.com>
2024-03-20 02:12:03 +0000

    implement uuid

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  before_create :assign_uuid
+
+  private
+
+  def assign_uuid
+    return unless has_attribute?(:id)
+
+    # UUID が衝突するくらいなら僕は宝くじが当たって億万長者になっててもおかしくない
+    self.id = SecureRandom.uuid
+  end
 end
